59db9b2f91f597fcc7bc6cd9d670127c6d6b0230
filippo-mancinelli <filipp.mancinelli@studenti.unicam.it>
2023-02-21 08:37:42 +0000

    keys

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 function handle_error() {
   if (( $? )) ; then
     echo -e "[\e[31mERROR\e[39m]"
@@ -9,21 +8,16 @@ function handle_error() {
     echo -e "[\e[32mOK\e[39m]"
   fi
 }
-
 ANSIBLE_FILES_DIR="$(dirname "$0")"
 INVENTORY="${1:-${ANSIBLE_FILES_DIR}/inventory.yml}"
-
 echo -n ">> Checking inventory file (${INVENTORY}) exists and is readable... "
 [ -r "${INVENTORY}" ]; handle_error "Please check https://github.com/w3f/polkadot-secure-validator/blob/master/GUIDE_ANSIBLE.md#inventory"
-
 echo -n ">> Pulling upstream changes... "
 out=$((git pull origin master) 2>&1)
 handle_error "$out"
-
 echo -n ">> Testing Ansible availability... "
 out=$((ansible --version) 2>&1)
 handle_error "$out"
-
 echo -n ">> Finding validator hosts... "
 out=$((ansible validator -i ${INVENTORY} --list-hosts) 2>/dev/null)
 if [[ $out == *"hosts (0)"* ]]; then
@@ -34,17 +28,14 @@ else
   echo -e "[\e[32mOK\e[39m]"
   echo "$out"
 fi
-
 echo -n ">> Testing connectivity to hosts... "
 out=$((ansible all -i ${INVENTORY} -m ping) 2>&1)
 handle_error "$out"
-
+echo "Sudo password for remote servers:"
+read -s SUDO_PW
 echo -n ">> Testing sudo access... "
-out=$((ansible all -i ${INVENTORY} -m ping --become --extra-vars "ansible_become_pass=pondsama") 2>&1)
+out=$((ansible all -i ${INVENTORY} -m ping --become --extra-vars "ansible_become_pass='$SUDO_PW'") 2>&1)
 handle_error "$out"
-
 echo ">> Executing Ansible Playbook..."
-
-ansible-playbook -i ${INVENTORY} ${ANSIBLE_FILES_DIR}/main.yml --become --extra-vars "ansible_become_pass=pondsama"
-
+ansible-playbook -i ${INVENTORY} ${ANSIBLE_FILES_DIR}/main.yml --become --extra-vars "ansible_become_pass='$SUDO_PW'"
 echo ">> Done!"
